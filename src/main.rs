5872aa4c826f6c56a172e425594890618bc6ac7e
Warren Hood <nullbyte001@gmail.com>
2023-08-23 00:18:45 +0200

    Fix help command

--- src/main.rs
+++ src/main.rs
@@ -45,8 +45,9 @@ impl EventHandler for Handler {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let framework = StandardFramework::new()
         .configure(|c| c.prefix("!")) // set the bot's prefix to "~"
-        .group(&GENERAL_GROUP)
-        .help(&HELP);
+        .help(&HELP)
+        .group(&GENERAL_GROUP);
+        
 
     // Login with a bot token from the environment
     let mut token: String = String::new();
